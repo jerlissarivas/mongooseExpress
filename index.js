adbd5958d52cd56faf2b16d2119d15bfd6dbcbaf
Jerlissa Rivas <jerlissa.rivas@gmail.com>
2021-02-24 11:02:43 -0500

    Install and require mongoose, express, and ejs

---
+++ index.js
@@ -0,0 +1,30 @@
+const express = require('express');
+const app = express();
+const path = require('path');
+const mongoose = require('mongoose');
+
+
+mongoose
+  .connect('mongodb://localhost/mongooseExpress', {
+    useCreateIndex: true,
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+  })
+  .then(() => {
+      console.log('CONNECTION SUCCESS!')
+  })
+  .catch(err => {
+      console.LOG('Error connecting to mongo', err);
+  })
+
+
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
+
+app.get('/dog', (req, res) => {
+    res.send('WOOF!')
+})
+
+app.listen(3000, () => {
+    console.log("APP IS LISTENING ON PORT 3000")
+})
