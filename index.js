17436059f5fa38bc23bc933506b3e8ee57a550fb
Jerlissa Rivas <jerlissa.rivas@gmail.com>
2021-02-24 14:59:15 -0500

    Update category selector for edit form

--- index.js
+++ index.js
@@ -1,61 +1,82 @@
-const express = require('express');
+const express = require("express");
 const app = express();
-const path = require('path');
-const mongoose = require('mongoose');
+const path = require("path");
+const mongoose = require("mongoose");
+const methodOverride = require("method-override");
 
-const Product = require('./models/product')
-
+const Product = require("./models/product");
 
 mongoose
-  .connect('mongodb://localhost/mongooseExpress', {
+  .connect("mongodb://localhost/mongooseExpress", {
     useCreateIndex: true,
     useNewUrlParser: true,
-    useUnifiedTopology: true
+    useUnifiedTopology: true,
   })
   .then(() => {
-      console.log('CONNECTION SUCCESS!')
+    console.log("CONNECTION SUCCESS!");
   })
-  .catch(err => {
-      console.LOG('Error connecting to mongo', err);
-  })
+  .catch((err) => {
+    console.LOG("Error connecting to mongo", err);
+  });
 
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "ejs");
+app.use(express.urlencoded({ extended: true }));
+app.use(methodOverride("_method"));
 
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'ejs');
-app.use(express.urlencoded({extended: true}))
+const categories = ["fruit", "vegetable", "dairy"];
 
 app.listen(3000, () => {
-    console.log("APP IS LISTENING ON PORT 3000")
-})
+  console.log("APP IS LISTENING ON PORT 3000");
+});
 
 // ROUTES
-// --- PRODUCT INDEX --- 
+// --- PRODUCT INDEX ---
 
-app.get('/products', async (req, res) => {
-    const products = await Product.find({})
-    res.render('products/index', { products })
-})
+app.get("/products", async (req, res) => {
+  const products = await Product.find({});
+  res.render("products/index", { products });
+});
 
 // --- SHOW NEW PRODUCT FORM ---
 
-app.get('/products/new', (req, res) => {
-    res.render('products/new')
-})
+app.get("/products/new", (req, res) => {
+  res.render("products/new", { categories });
+});
 
 // --- POST NEW PRODUCT FORM ---
 
-app.post('/products', async (req, res) => {
-    const newProduct = new Product(req.body)
-    await newProduct.save()
-    console.log(newProduct)
-    res.redirect(`/products/${newProduct._id}`)
-})
+app.post("/products", async (req, res) => {
+  const newProduct = new Product(req.body);
+  await newProduct.save();
+  console.log(newProduct);
+  res.redirect(`/products/${newProduct._id}`);
+});
 
 // --- PRODUCT DETAILS ---
 
-app.get('/products/:id', async (req, res) => {
-    const { id } = req.params;
-    const product = await Product.findById(id)
-    console.log(product);
-    res.render('products/details', { product })
-})
+app.get("/products/:id", async (req, res) => {
+  const { id } = req.params;
+  const product = await Product.findById(id);
+  console.log(product);
+  res.render("products/details", { product });
+});
+
+// --- SHOW EDIT PRODUCT FORM ---
+
+app.get("/products/:id/edit", async (req, res) => {
+  const { id } = req.params;
+  const product = await Product.findById(id);
+  res.render("products/edit", { product, categories });
+});
+
+// --- SUBMIT EDIT FORM ---
+
+app.put("/products/:id", async (req, res) => {
+  const { id } = req.params;
+  const product = await Product.findByIdAndUpdate(id, req.body, {
+    runValidators: true,
+    new: true,
+  });
+  res.redirect(`/products/${product._id}`);
+});
--- seeds.js
+++ seeds.js
@@ -1,52 +1,51 @@
-const mongoose = require('mongoose');
-const Product = require('./models/product')
-
+const mongoose = require("mongoose");
+const Product = require("./models/product");
 
 mongoose
-  .connect('mongodb://localhost/mongooseExpress', {
+  .connect("mongodb://localhost/mongooseExpress", {
     useCreateIndex: true,
     useNewUrlParser: true,
-    useUnifiedTopology: true
+    useUnifiedTopology: true,
   })
   .then(() => {
-      console.log('CONNECTION SUCCESS!')
+    console.log("CONNECTION SUCCESS!");
   })
-  .catch(err => {
-      console.LOG('Error connecting to mongo', err);
-  })
+  .catch((err) => {
+    console.LOG("Error connecting to mongo", err);
+  });
 
 const seedProducts = [
-    {
-    name: 'Ruby Grapefruit',
+  {
+    name: "Ruby Grapefruit",
     price: 1.99,
-    category: 'fruit'
-    },
-    {
-    name: 'Banana',
+    category: "fruit",
+  },
+  {
+    name: "Banana",
     price: 0.99,
-    category: 'fruit'
-    },
-    {
-    name: 'Tomato',
+    category: "fruit",
+  },
+  {
+    name: "Tomato",
     price: 2.19,
-    category: 'vegetable'
-    },
-    {
-    name: 'Celery',
+    category: "vegetable",
+  },
+  {
+    name: "Celery",
     price: 2.89,
-    category: 'vegetable'
-    },
-    {
-    name: 'Milk',
+    category: "vegetable",
+  },
+  {
+    name: "Milk",
     price: 5.99,
-    category: 'dairy'
-    }
-]
+    category: "dairy",
+  },
+];
 
 Product.insertMany(seedProducts)
-    .then(res => {
-        console.log(res)
-    })
-    .catch(e => {
-        console.log(e)
-    })
+  .then((res) => {
+    console.log(res);
+  })
+  .catch((e) => {
+    console.log(e);
+  });
