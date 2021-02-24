335b82d8c21246d2697d3e3d76c7fd28ca8f9a59
Jerlissa Rivas <jerlissa.rivas@gmail.com>
2021-02-24 13:16:04 -0500

    Add index and details route

--- index.js
+++ index.js
@@ -23,10 +23,23 @@ mongoose
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
-app.get('/dog', (req, res) => {
-    res.send('WOOF!')
-})
-
 app.listen(3000, () => {
     console.log("APP IS LISTENING ON PORT 3000")
+})
+
+// ROUTES
+// --- PRODUCT INDEX --- 
+
+app.get('/products', async (req, res) => {
+    const products = await Product.find({})
+    res.render('products/index', { products })
+})
+
+// --- PRODUCT DETAILS ---
+
+app.get('/products/:id', async (req, res) => {
+    const { id } = req.params;
+    const product = await Product.findById(id)
+    console.log(product);
+    res.render('products/details', { product })
 })
